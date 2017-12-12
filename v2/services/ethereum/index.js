f5d9917b9990a5bce72b02d3db7401ebfed96b9e
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-12-12 15:57:52 +0700

    sync with parity instead of geth

--- v2/services/ethereum/index.js
+++ v2/services/ethereum/index.js
@@ -1,19 +1,10 @@
 const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://ethereum_node:8546'))
 
-setTimeout(() => {
-  try {
-    // const web1 = new Web3(new Web3.providers.WebsocketProvider('ws://ethereum_node:8546'))
-    // const web2 = new Web3(new Web3.providers.WebsocketProvider('ws://mesg_ethereum_node:8546'))
-    const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://188.166.234.120:8546'))
-    
-    web3.eth.subscribe('newBlockHeaders', (err, result) => {
-      if (err) { console.error(err) }
-      console.log(result.number)
-    })
-  } catch (e) {
-    console.error("ERRROR")
-    console.error(e)
-  }
-}, 10000)
-
-setInterval(() => console.log('ping'), 1000)
+const subscription = web3.eth.subscribe('newBlockHeaders', (err, result) => {
+  if (err) { console.error('Error on subscribe', err) }
+})
+subscription
+  .on('changed', () => console.log(`Websocket changed`))
+  .on('error', error => console.error('error on ethereum subscription', error))
+  .on('data', block => console.log(block.number))
