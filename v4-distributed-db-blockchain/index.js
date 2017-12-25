ed30f7e699b7396f969b9c4e64c4323a5ac578cb
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-12-25 22:06:33 +0700

    add genesis and scripts to run and test

--- v4-distributed-db-blockchain/index.js
+++ v4-distributed-db-blockchain/index.js
@@ -1,17 +1,30 @@
 const Web3 = require('web3')
 
-const client = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8546'))
+const client = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+const authority = '0x1ea3b231019f14ba0c24b428dbb42d12f97cac8d'
+const user = '0xf4cd771183e292506288dcc7a18aae9f56ea847c'
 
 const start = async () => {
   const addresses = await client.eth.getAccounts()
+  // client.eth.personal.
   console.log(addresses)
-  const subscription = await client.eth.subscribe('newBlockHeaders', (err, result) => {
-    if (err) { Logger.error('Error on subscribe', { blockchain, err }) }
-  })
-  subscription
-    .on('changed', () => console.error(`Websocket changed`))
-    .on('error', error => { throw error })
-    .on('data', block => console.log(block.number))
+  console.log(await client.eth.getBalance(authority))
+  console.log(await client.eth.getBalance(user))
+  // debugger
+  // const res = await client.eth.sendTransaction({
+  //   from: authority,
+  //   to: user,
+  //   value: client.utils.toWei('1', 'ether')
+  // })
+  // const res = await client.eth.sendSignedTransaction(signedTransaction)
+  // const subscription = await client.eth.subscribe('newBlockHeaders', (err, result) => {
+  //   if (err) { Logger.error('Error on subscribe', { blockchain, err }) }
+  // })
+  // subscription
+  //   .on('changed', () => console.error(`Websocket changed`))
+  //   .on('error', error => { throw error })
+  //   .on('data', block => console.log(block.number))
 }
 
 start()
--- v4-distributed-db-blockchain/start.sh
+++ v4-distributed-db-blockchain/start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 $GETH_PATH/geth \
+  --networkid 37173 \
   --datadir ./data \
-  --identity MESG \
   --rpc \
-  --rpcapi db,eth,net,web3,personal \
-  --rpccorsdomain "*" \
-  --ws \
-  --wsorigins "*" \
-  --ipcdisable \
-  --nodiscover \
-  --mine \
-  --minerthreads 1
+  --identity Antho \
+  --cache 1024 \
+  --syncmode full \
+  --bootnodes enode://406d2754ad66dcfe227ce3b27ed33353edc7fb3090276c10e2bec7b30f1a0b8ef2f4c9058dee2e22a8a3f1369e590c7ab9b936c7616b638e456a22a8ce5b1ec6@174.138.16.123:30303
+#  --ethstats yournode:MESG-test@laughing-murdock-37ecaf.netlify.com \
+ # --bootnodes enode://cc936b11ce49018a65c94cc8fe601dbc0ea5d8123bccd6e142e5ade5decb7c5aec6bb2c72583676f0728427793acec8b39c236e637ae28b04e6e2ad523df4644@174.138.31.131:30303
+  #--bootnodes enode://8ca4fb1b3e5308c43b8bc6a30e29bd79a1209e989a221b88f58013fa564d51b4ea0201f2a8b26dd751b4aafe81dbec15b9c2d546643b848aefc12221b11be30f@139.59.114.186:30301 \
