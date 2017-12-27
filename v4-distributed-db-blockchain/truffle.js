0c6e13c2ed0c3467074e978792c7a03da665e893
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-12-27 12:59:23 +0700

    add truffle config

--- v4-distributed-db-blockchain/truffle.js
+++ v4-distributed-db-blockchain/truffle.js
@@ -3,7 +3,11 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "5777"
+      network_id: "5777",
+      gas: 5000000
     }
+  },
+  mocha: {
+    useColors: true
   }
 };
