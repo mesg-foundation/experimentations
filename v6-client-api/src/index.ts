0cdee15dcb495d88b41005a1215fcc8a0336f652
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-01-27 10:46:04 +0700

    use prettier formatter

--- v6-client-api/src/index.ts
+++ v6-client-api/src/index.ts
@@ -1,18 +1,18 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Prisma } from './generated/prisma'
-import resolvers from './resolvers'
+import { GraphQLServer } from "graphql-yoga";
+import { Prisma } from "./generated/prisma";
+import resolvers from "./resolvers";
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
-      endpoint: 'http://localhost:4466/v6-client-api/dev', // the endpoint of the Prisma DB service
-      secret: 'mysecret123', // specified in database/prisma.yml
-      debug: true, // log all GraphQL queries & mutations
+      endpoint: "http://localhost:4466/v6-client-api/dev", // the endpoint of the Prisma DB service
+      secret: "mysecret123", // specified in database/prisma.yml
+      debug: true // log all GraphQL queries & mutations
     })
-  }),
-})
+  })
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log("Server is running on http://localhost:4000"));
--- v6-client-api/src/resolvers.ts
+++ v6-client-api/src/resolvers.ts
@@ -1,36 +1,45 @@
-import { Context } from './utils'
-import { Runner, Workflow } from './generated/prisma'
+import { Context } from "./utils";
+import { Runner, Workflow } from "./generated/prisma";
 
-const delegateQuery = query => (parent, args, ctx: Context, info) => ctx.db
-  .query[query](args, info)
-const delegateMutation = mutation => (parent, args, ctx: Context, info) => ctx.db
-  .mutation[mutation](args, info)
+const delegateQuery = query => (parent, args, ctx: Context, info) =>
+  ctx.db.query[query](args, info);
+const delegateMutation = mutation => (parent, args, ctx: Context, info) =>
+  ctx.db.mutation[mutation](args, info);
 
-const delegate = (mapping, func) => Object.keys(mapping)
-  .reduce((acc, x) => ({
-    ...acc,
-    [x]: func(mapping[x])
-  }), {})
+const delegate = (mapping, func) =>
+  Object.keys(mapping).reduce(
+    (acc, x) => ({
+      ...acc,
+      [x]: func(mapping[x])
+    }),
+    {}
+  );
 
 export default {
   Query: {
-    ...delegate({
-      runner: 'runner',
-      runners: 'runners',
-      workflow: 'workflow',
-      workflows: 'workflows',
-      service: 'service',
-      services: 'services',
-    }, delegateQuery),
+    ...delegate(
+      {
+        runner: "runner",
+        runners: "runners",
+        workflow: "workflow",
+        workflows: "workflows",
+        service: "service",
+        services: "services"
+      },
+      delegateQuery
+    )
   },
-  
+
   Mutation: {
-    ...delegate({
-      deployWorkflow: 'createWorkflow',
-      disableWorkflow: 'deleteWorkflow',
-      register: 'createRunner',
-      unregister: 'deleteRunner',
-      createService: 'createService',
-    }, delegateMutation),
+    ...delegate(
+      {
+        deployWorkflow: "createWorkflow",
+        disableWorkflow: "deleteWorkflow",
+        register: "createRunner",
+        unregister: "deleteRunner",
+        createService: "createService"
+      },
+      delegateMutation
+    )
   }
-}
+};
